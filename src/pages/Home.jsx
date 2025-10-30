e9d63e8975a31131435c65ddea737ed182f7c13b
An <anvu.1296@gmail.com>
2025-10-29 20:00:29 -0500

    move index.html to root

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,4 @@
 // src/pages/Home.jsx
-import books from '../data/books.json';
-import BookCard from '../components/BookCard';
-
 export default function Home() {
-    return (
-        <div>
-            <h1>Bookshelf</h1>
-            {books.map(book => (
-                <BookCard key={book.id} book={book} />
-            ))}
-        </div>
-    );
+  return <div>Home Page</div>;
 }
