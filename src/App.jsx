3901a3923f24b289c029929257caf9c4976d69d9
An <anvu.1296@gmail.com>
2025-11-04 04:31:47 -0600

    fix browser router

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Reader from './pages/Reader';
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Reader from "./pages/Reader";
 
 export default function App() {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/reader/:bookId" element={<Reader />} />
-            </Routes>
-        </BrowserRouter>
-    );
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/reader/:bookId" element={<Reader />} />
+    </Routes>
+  );
 }
