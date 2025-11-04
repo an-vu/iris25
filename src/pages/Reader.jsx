6d35c9d94d4672573db0783ce0e83f9c235c2486
An <anvu.1296@gmail.com>
2025-11-03 18:35:00 -0600

    working on reader container

--- src/pages/Reader.jsx
+++ src/pages/Reader.jsx
@@ -1,21 +1,19 @@
 import { useParams } from "react-router-dom";
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import { books } from "../data/books.js";
 
 import "../styles/Reader.css";
 import "../styles/MasterContainer.css";
-import "../styles/BookCard.css";
 import "../styles/Navbar.css";
-import "../styles/Button.css";
 import "../styles/ButtonNavbar.css";
 import "../styles/ReaderTitle.css";
 import "../styles/ReaderContainer.css";
 
 import NavbarReader from "../components/NavbarReader.jsx";
+import ReaderContainer from "../components/ReaderContainer.jsx";
 
 export default function Reader() {
   const { bookId } = useParams();
-  const [page, setPage] = useState(1);
   const currentBook = books[bookId] || {};
 
   useEffect(() => {
@@ -28,20 +26,12 @@ export default function Reader() {
 
         {/* Title Section */}
         <div className="title-container">
-          <h4 className="title">
-            {currentBook.title}
-          </h4>
-          <h4 className="author">
-            {currentBook.author}
-          </h4>
+          <h4 className="title">{currentBook.title}</h4>
+          <h4 className="author">{currentBook.author}</h4>
         </div>
 
-        {/* Reader Area (placeholder for PDF) */}
-        <div className="reader-container">
-          <div className="pdf-placeholder">
-            <p>PDF Viewer Placeholder</p>
-          </div>
-        </div>
+        {/* Reader Area */}
+        <ReaderContainer />
 
         {/* Bottom Navbar */}
         <NavbarReader />
